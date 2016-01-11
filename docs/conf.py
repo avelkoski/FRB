5f99374bacf449e7b1095681b00100b40025be36
Aleksandar Velkoski <avelkoski@ltu.edu>
2016-01-10 18:09:53 -0600

    comment sys path

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 # -- General configuration ------------------------------------------------
 
@@ -55,7 +55,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'FRED'
+project = u'FRB'
 copyright = u'2016, Aleksandar Velkoski'
 author = u'Aleksandar Velkoski'
 
